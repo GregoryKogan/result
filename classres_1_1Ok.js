c0c0f7dff16f9f197b3dfe0a45bf1e011cb457e1
Gregory Koganovskiy <grisha.koganovskiy@gmail.com>
2023-12-26 00:39:08 +0300

    Update classres_1_1Ok.js

--- classres_1_1Ok.js
+++ classres_1_1Ok.js
@@ -1,5 +1,6 @@
 var classres_1_1Ok =
 [
+    
     [ "Ok", "classres_1_1Ok.html#aa4d6e6dacbe21d4e291efc3c08e0045b", null ],
     [ "Ok", "classres_1_1Ok.html#a0a321d009597a5284ca1c90125fb9ed2", null ],
     [ "Ok", "classres_1_1Ok.html#a166c9acb93b64c27c7fafe112b604a15", null ],
