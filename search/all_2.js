3d4d5b3773fbcc5fb18651266cab0eb739628d2b
GregoryKogan <grisha.koganovskiy@gmail.com>
2023-12-26 17:10:35 +0000

    Deploying to gh-pages from @ GregoryKogan/result-cpp@158b8952dce690790d319acfc9e0ac9435b8e877 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['ok_0',['Ok',['../classres_1_1Ok.html#aa4d6e6dacbe21d4e291efc3c08e0045b',1,'res::Ok::Ok()'],['../classres_1_1Ok.html#a0a321d009597a5284ca1c90125fb9ed2',1,'res::Ok::Ok(const T &amp;value)'],['../classres_1_1Ok.html#a166c9acb93b64c27c7fafe112b604a15',1,'res::Ok::Ok(T &amp;&amp;value)'],['../classres_1_1Ok.html',1,'res::Ok&lt; T &gt;']]],
-  ['operator_20result_3c_20t_2c_20e_20_3e_1',['operator Result&lt; T, E &gt;',['../classres_1_1Ok.html#af1ef17effc3000b44c0b2fb42be0abbc',1,'res::Ok::operator Result&lt; T, E &gt;()'],['../classres_1_1Err.html#ab49f16277322b7085881fa2781a16cc4',1,'res::Err::operator Result&lt; T, E &gt;()']]]
+  ['operator_20bool_1',['operator bool',['../classres_1_1Result.html#a4d36ea9a547135154ddf9f8380620914',1,'res::Result']]],
+  ['operator_20result_3c_20t_2c_20e_20_3e_2',['operator Result&lt; T, E &gt;',['../classres_1_1Ok.html#af1ef17effc3000b44c0b2fb42be0abbc',1,'res::Ok::operator Result&lt; T, E &gt;()'],['../classres_1_1Err.html#ab49f16277322b7085881fa2781a16cc4',1,'res::Err::operator Result&lt; T, E &gt;()']]],
+  ['operator_2a_3',['operator*',['../classres_1_1Result.html#ae73befde53f79ffe648fc18968e21f09',1,'res::Result::operator*() const &amp;-&gt; const T &amp;'],['../classres_1_1Result.html#ab3589917ac7d6f4222c7a8a3a1b18a36',1,'res::Result::operator*() &amp;-&gt; T &amp;'],['../classres_1_1Result.html#ad2b49d11ab7e081f820a89f4a2925576',1,'res::Result::operator*() const &amp;&amp;-&gt; const T &amp;&amp;'],['../classres_1_1Result.html#a69cc780d0613d3b7889bee91d876e883',1,'res::Result::operator*() &amp;&amp;-&gt; T &amp;&amp;']]],
+  ['operator_2d_3e_4',['operator-&gt;',['../classres_1_1Result.html#a2e7eaa369b1454f19bfc3d4f86915319',1,'res::Result::operator-&gt;() const -&gt; const T *'],['../classres_1_1Result.html#a079ff259ddb9fc666b2b07ceb4f7356b',1,'res::Result::operator-&gt;() -&gt; T *']]]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['unwrap_0',['unwrap',['../classres_1_1Result.html#aacd3069c9335ee26efb96ac814199688',1,'res::Result']]],
-  ['unwrap_5ferr_1',['unwrap_err',['../classres_1_1Result.html#ae62cbc23e0435b8da8674d22ae27b643',1,'res::Result']]]
+  ['unwrap_5ferr_1',['unwrap_err',['../classres_1_1Result.html#ae62cbc23e0435b8da8674d22ae27b643',1,'res::Result']]],
+  ['unwrap_5for_2',['unwrap_or',['../classres_1_1Result.html#acff05ca6bf1244c8632e58007bb35f84',1,'res::Result']]]
 ];
