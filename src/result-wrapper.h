7dfe94d06f9520b6048f86619cff77608bfcaab5
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-01-08 00:56:06 +0300

    Merge all source files into single header

---
+++ src/result-wrapper.h
@@ -0,0 +1,15 @@
+/// result is a C++ library that provides a Result<T, E> type, which can be used to return and propagate errors. It's
+/// inspired by Rust's std::Result type.
+///
+/// author: GregoryKogan
+/// My github page: https://github.com/GregoryKogan
+/// version: 0.0.0
+/// date: 2023, December 21
+/// This software is released under the GNU GENERAL PUBLIC LICENSE Version 3.
+
+#ifndef RESULT_LIB
+#define RESULT_LIB
+
+#include "result/result.hpp"
+
+#endif // RESULT_LIB
