ca048d172ecff6a2ef8286ebca29430f44dc5a45
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-01-08 01:45:42 +0300

    Add tests for content getters

---
+++ tests/content_getters.cpp
@@ -0,0 +1,30 @@
+#include "../result.h"
+#include <gtest/gtest.h>
+#include <string>
+
+TEST(ContentGetters, Ok) {
+  const int value = 42;
+  res::result<int, std::string> result = res::ok(value);
+  EXPECT_EQ(result.value(), 42);
+  EXPECT_THROW(auto val = result.error(), std::logic_error); // NOLINT
+}
+
+TEST(ContentGetters, Err) {
+  const std::string error = "error";
+  res::result<int, std::string> result = res::err(error);
+  EXPECT_THROW(auto val = result.value(), std::logic_error); // NOLINT
+  EXPECT_EQ(result.error(), "error");
+}
+
+TEST(ContentGetters, VoidOk) {
+  res::result<void, std::string> result = res::ok();
+  EXPECT_THROW(auto val = result.error(), std::logic_error); // NOLINT
+  // EXPECT_NO_THROW(auto val = result.value()); - Should not compile
+}
+
+TEST(ContentGetters, VoidErr) {
+  const std::string error = "error";
+  res::result<void, std::string> result = res::err(error);
+  EXPECT_EQ(result.error(), "error");
+  // EXPECT_THROW(auto val = result.value(), std::logic_error); - Should not compile
+}
