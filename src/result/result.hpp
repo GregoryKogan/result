e557444d1deaba93642c5de8d1a76af5b12e1fb6
GregoryKogan <grisha.koganovskiy@gmail.com>
2024-01-09 19:48:15 +0300

    Use std::variant and std::optional to hold values of result

--- src/result/result.hpp
+++ src/result/result.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <optional>
 #include <stdexcept>
 #include <utility>
+#include <variant>
 
 #include "../err/err.hpp"
 #include "../ok/ok.hpp"
@@ -18,31 +20,20 @@ template <typename T, typename E> class result {
   static_assert(!std::is_same_v<T, void>, "T (value type) must not be void");
   static_assert(!std::is_same_v<E, void>, "E (error type) must not be void");
 
-  bool successful_;
-  T value_;
-  E error_;
+  std::variant<T, E> content_;
 
   // Simulate named constructors
   enum Successful { SUCCESSFUL };
   enum Unsuccessful { UNSUCCESSFUL };
-  result(Successful successful, T value) : successful_(true), value_(std::move(value)) {}
-  result(Unsuccessful unsuccessful, E error) : successful_(false), error_(std::move(error)) {}
-
-  auto make_successful(T value) -> void {
-    successful_ = true;
-    value_ = std::move(value);
-  }
-  auto make_unsuccessful(E error) -> void {
-    successful_ = false;
-    error_ = std::move(error);
-  }
+  result(Successful successful, T value) { content_.template emplace<0>(std::move(value)); }
+  result(Unsuccessful unsuccessful, E error) { content_.template emplace<1>(std::move(error)); }
 
   template <typename U> friend class ok;
   template <typename U> friend class err;
 
 public:
   // Observers
-  [[nodiscard]] auto is_ok() const -> bool { return successful_; }
+  [[nodiscard]] auto is_ok() const -> bool { return content_.index() == 0; }
   explicit operator bool() const { return is_ok(); }
   auto operator!() const -> bool { return !is_ok(); }
 
@@ -55,15 +46,15 @@ public:
   // result object. If the result object is an error, the functor is not called and the error is propagated. But the
   // result value type is changed to U.
   template <typename F, typename U = std::invoke_result_t<F, T>> auto map(F &&functor) const -> result<U, E> {
-    if (is_ok()) { return ok<U>(std::forward<F>(functor)(value_)); }
-    return err<E>(error_);
+    if (is_ok()) { return ok<U>(std::forward<F>(functor)(value())); }
+    return err<E>(error());
   }
   // map_err method gets functor F(E x) -> R as an argument and returns result of applying this functor to the error of
   // the result object. If the result object is a success, the functor is not called and the value is propagated. But
   // the result error type is changed to R.
   template <typename F, typename R = std::invoke_result_t<F, E>> auto map_err(F &&functor) const -> result<T, R> {
-    if (!is_ok()) { return err<R>(std::forward<F>(functor)(error_)); }
-    return ok<T>(value_);
+    if (!is_ok()) { return err<R>(std::forward<F>(functor)(error())); }
+    return ok<T>(value());
   }
 };
 
@@ -71,21 +62,20 @@ public:
 template <typename E> class result<void, E> {
   static_assert(!std::is_same_v<E, void>, "E (error type) must not be void");
 
-  bool successful_;
-  E error_;
+  std::optional<E> error_;
 
   // Simulate named constructors
   enum Successful { SUCCESSFUL };
   enum Unsuccessful { UNSUCCESSFUL };
-  explicit result(Successful successful) : successful_(true) {}
-  result(Unsuccessful unsuccessful, E error) : successful_(false), error_(std::move(error)) {}
+  explicit result(Successful successful) {}
+  result(Unsuccessful unsuccessful, E error) : error_(std::move(error)) {}
 
   template <typename U> friend class ok;
   template <typename U> friend class err;
 
 public:
   // Observers
-  [[nodiscard]] auto is_ok() const -> bool { return successful_; }
+  [[nodiscard]] auto is_ok() const -> bool { return !error_.has_value(); }
   explicit operator bool() const { return is_ok(); }
   auto operator!() const -> bool { return !is_ok(); }
 
@@ -97,33 +87,33 @@ public:
   // result value type is changed to U.
   template <typename F, typename U = std::invoke_result_t<F>> auto map(F &&functor) const -> result<U, E> {
     if (is_ok()) { return ok<U>(std::forward<F>(functor)()); }
-    return err<E>(error_);
+    return err<E>(error_.value());
   }
   // map_err method for void value type is same as for non-void value type because it does not depend on the value type.
   template <typename F, typename R = std::invoke_result_t<F, E>> auto map_err(F &&functor) const -> result<void, R> {
-    if (!is_ok()) { return err<R>(std::forward<F>(functor)(error_)); }
+    if (!is_ok()) { return err<R>(std::forward<F>(functor)(error_.value())); }
     return ok();
   }
 };
 
 template <typename T, typename E> inline auto result<T, E>::value() const -> const T & {
   if (!is_ok()) { throw std::logic_error("value() called on result with error"); }
-  return value_;
+  return std::get<0>(content_);
 }
 
 template <typename T, typename E> inline auto result<T, E>::value_or(T &&default_value) const -> T {
-  if (is_ok()) { return value_; }
+  if (is_ok()) { return std::get<0>(content_); }
   return std::move(default_value);
 }
 
 template <typename T, typename E> inline auto result<T, E>::error() const -> const E & {
   if (is_ok()) { throw std::logic_error("error() called on result with value"); }
-  return error_;
+  return std::get<1>(content_);
 }
 
 template <typename E> inline auto result<void, E>::error() const -> const E & {
   if (is_ok()) { throw std::logic_error("error() called on result with value"); }
-  return error_;
+  return error_.value();
 }
 
 } // namespace res
